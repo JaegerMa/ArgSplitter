2d85a1aa447131b8271e99cacb4d55a2c623f17f
JaegerMa <JaegerMa.Git@lightning-mail.de>
2018-05-05 22:17:20 +0200

    Initial commit

---
+++ ArgSplitter/ArgSplitter.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ArgSplitter
+{
+	public static class ArgSplitter
+	{
+		public static IEnumerable<string> SplitArgs(this string input, int maxParts = -1, bool removeAllEscapeSequences = false)
+		{
+			var chars = input.Trim().ToCharArray().ToList();
+			var fragments = new List<string>();
+
+			int parts = 0;
+			int nextFragmentStart = 0;
+			bool inBounds = false;
+
+			for(int i = 0; i < chars.Count; ++i)
+			{
+				var c = chars[i];
+				if(c == '\\')
+				{
+					if(!removeAllEscapeSequences && (i + 1 >= chars.Count || !isEscapeable(chars[i + 1])))
+						continue;
+
+					chars.RemoveAt(i);
+					continue;
+				}
+
+				//Interpret " only as start or end of a single argument,
+				//if it's not in the middle of a string
+				if(c == '"' && (!inBounds
+								? i == nextFragmentStart
+								: (i + 1 == chars.Count || isSpace(chars[i + 1]))))
+				{
+					inBounds = !inBounds;
+					chars.RemoveAt(i);
+					--i;
+					continue;
+				}
+
+				if(inBounds)
+					continue;
+
+				if(isSpace(c))
+				{
+					AddFragment(nextFragmentStart, i);
+					nextFragmentStart = i + 1;
+
+					//Create maxParts - 1 parts as the last part isn't added here
+					if(++parts + 1 >= maxParts)
+						break;
+				}
+			}
+
+			if(nextFragmentStart < chars.Count)
+				AddFragment(nextFragmentStart);
+
+			return fragments;
+
+
+			bool isSpace(char c)
+			{
+				return c == ' ' || c == '\t';
+			}
+			bool isEscapeable(char c)
+			{
+				switch(c)
+				{
+					case ' ':
+					case '"':
+					case '\\':
+						return true;
+					default:
+						return false;
+				}
+			}
+			void AddFragment(int start, int end = -1)
+			{
+				if(end <= start && end >= 0)
+					return;
+
+				if(end < 0)
+					end = chars.Count;
+
+				var fragment = string.Join("", chars.GetRange(start, end - start));
+				fragments.Add(fragment);
+			}
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,36 @@
+# ArgSplitter
+
+ArgSplitter is a small .NET Standard 2.0 compatible library to split a string into command-line args considering escape sequences and quotes
+
+
+## Usage
+ArgSplitter extends the string type and adds the method `SplitArgs`.
+```csharp
+//Input string: arg1 arg2 "arg3 arg3 arg3" arg4\ arg4 arg5
+string myArgString = "arg1 arg2 \"arg3 arg3 arg3\" arg4\\ arg4 arg5";
+
+//Call using extension method
+string[] args = myArgString.SplitArgs();
+
+//Call using ArgSplitter-class
+string[] args = ArgSplitter.SplitArgs(myArgString);
+
+
+//args: ["arg1", "arg2", "arg3 arg3 arg3", "arg4 arg4", "arg5"]
+```
+
+## `SplitArgs` method
+#### Args
+- `input`: string
+- `maxParts`: int, default `-1`  
+If greater than 0, string will be splitted in `maxParts` max
+- `removeAllEscapeSequences`: bool, default `false`  
+By default, only space, `"` and `\` are escaped. All other backslashes are kept. If `true` is passed, all (not escaped) backslashes will be removed.  
+**Note**: Known escape sequences like `\t`, `\r` or `\n` aren't resolved! The backslashes are just removed.
+
+#### Returns
+`IEnumerable<string>` containing the arguments
+
+
+## License
+ArgSplitter is licensed under the MIT License
